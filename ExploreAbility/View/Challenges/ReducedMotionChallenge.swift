7e9a54fad67bac5452fe6b60f55b087cff23125d
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-04-26 18:58:16 +0700

    finish reduced motion challenge

--- ExploreAbility/View/Challenges/ReducedMotionChallenge.swift
+++ ExploreAbility/View/Challenges/ReducedMotionChallenge.swift
@@ -11,9 +11,99 @@ struct ReducedMotionChallenge: View {
     
     var namespace: Namespace.ID
     
+    @State var onSucceedCalled = false
     var onSucceed: (() -> ())?
     
+    private let publisher = NotificationCenter.default.publisher(for: UIAccessibility.reduceMotionStatusDidChangeNotification)
+    
+    @State private var startRandomMove = false
+    
+    @State private var isDropped = false
+    
+    @State private var state = ReducedMotionState.started
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { context in
+            ZStack {
+                switch state {
+                case .started:
+                    ForEach(0..<20) { index in
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.red)
+                            .frame(width: .random(in: context.size.width / 2..<context.size.height / 2), height: .random(in: context.size.width / 2..<context.size.height / 2))
+                            .offset(x: .random(in: -context.size.width / 2..<context.size.width / 2) + (startRandomMove ? .random(in: -context.size.width / 2..<context.size.width / 2) : 0),
+                                    y: .random(in: -context.size.height / 2..<context.size.height / 2) + (startRandomMove ? .random(in: -context.size.width / 2..<context.size.width / 2) : 0))
+                            .opacity(0.5)
+                            .matchedGeometryEffect(id: "ReducedMotion\(index)", in: namespace)
+                    }
+                case .reducedMotionEnabled:
+                    VStack {
+                        ZStack {
+                            ForEach(0..<20) { index in
+                                Circle()
+                                    .fill(.red)
+                                    .frame(width: 20, height: 20)
+                                    .matchedGeometryEffect(id: "ReducedMotion\(index)", in: namespace)
+                            }
+                            
+                            Circle()
+                                .fill(.red)
+                                .frame(width: 20, height: 20)
+                                .matchedGeometryEffect(id: GameState.reducedMotion, in: namespace)
+                        }
+                        
+                        Text("You did it!\nNow, turn Reduced Motion off.")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                            .offset(y: state == .reducedMotionEnabled ? -300 : 300)
+                    }
+                case .reducedMotionDisabled:
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 20, height: 20)
+                        .matchedGeometryEffect(id: GameState.reducedMotion, in: namespace)
+                        .offset(y: isDropped ? context.size.height / 2 - 42 : 0)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 0.5).repeatForever(autoreverses: true)) {
+                startRandomMove.toggle()
+            }
+        }
+        .onReceive(publisher) { _ in
+            
+            if UIAccessibility.isReduceMotionEnabled && state == .started {
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                    withAnimation {
+                        state = .reducedMotionEnabled
+                    }
+                }
+            } else if !UIAccessibility.isReduceMotionEnabled && state == .reducedMotionEnabled {
+                withAnimation {
+                    state = .reducedMotionDisabled
+                }
+                
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.3)) {
+                        isDropped = true
+                    }
+                    
+                    Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+                        if !onSucceedCalled {
+                            onSucceed?()
+                            onSucceedCalled = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    enum ReducedMotionState {
+        case started
+        case reducedMotionEnabled
+        case reducedMotionDisabled
     }
 }
