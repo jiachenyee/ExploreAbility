dfb01b4533f816cf06c113ace7de8fcb4069238b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-05-05 00:11:10 +0700

    refactor view model

---
+++ ExploreAbility/View Model/Multipeer Connectivity/ViewModel+Messages.swift
@@ -0,0 +1,73 @@
+//
+//  ViewModel+Messages.swift
+//  ExploreAbility
+//
+//  Created by Jia Chen Yee on 5/5/23.
+//
+
+import Foundation
+import MultipeerConnectivity
+import SwiftUI
+
+extension ViewModel {
+    func sendHelloMessage() {
+        guard let hostPeerID else { return }
+        
+        let helloMessage = HelloClientMessage(groupName: groupName)
+        do {
+            let data = try ClientMessage(payload: .hello(helloMessage)).toData()
+            
+            try mcSession.send(data, toPeers: [hostPeerID], with: .reliable)
+            
+            withAnimation {
+                gameState = .waitingRoom
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func sendHeartbeatMessage() {
+        guard abs((lastHeatbeatMessageDate ?? .distantPast).timeIntervalSinceNow) > 1,
+              let hostPeerID,
+              let ipsPosition else {
+            if !heartbeatScheduled {
+                heartbeatScheduled = true
+                
+                let newDate = lastHeatbeatMessageDate!.advanced(by: 1.001)
+                
+                Timer.scheduledTimer(withTimeInterval: abs(newDate.timeIntervalSinceNow), repeats: false) { _ in
+                    self.sendHeartbeatMessage()
+                }
+            }
+            return
+        }
+        
+        heartbeatScheduled = false
+        
+        lastHeatbeatMessageDate = .now
+        
+        let heartbeat = HeartbeatClientMessage(gameState: gameState, ipsPosition: ipsPosition)
+        
+        do {
+            let data = try ClientMessage(payload: .heartbeat(heartbeat)).toData()
+            
+            try mcSession.send(data, toPeers: [hostPeerID], with: .reliable)
+        } catch {
+            print(error.localizedDescription)
+        }
+        print(heartbeat.ipsPosition)
+    }
+    
+    func sendChallengeStartedMessage() {
+        guard let hostPeerID else { return }
+        
+        do {
+            let data = try ClientMessage(payload: .challengeStarted(ChallengeStartedClientMessage(gameState: gameState, date: .now))).toData()
+            
+            try mcSession.send(data, toPeers: [hostPeerID], with: .reliable)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
