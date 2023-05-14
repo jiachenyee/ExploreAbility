a12d18a806a53ceb6afeb074e309bffed3c64efe
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-05-14 22:36:38 +0700

    lock interface to light mode

--- ExploreAbility/ExploreAbilityApp.swift
+++ ExploreAbility/ExploreAbilityApp.swift
@@ -14,6 +14,7 @@ struct ExploreAbilityApp: App {
             ContentView()
                 .persistentSystemOverlays(.hidden)
                 .statusBarHidden()
+                .preferredColorScheme(.light)
         }
     }
 }
