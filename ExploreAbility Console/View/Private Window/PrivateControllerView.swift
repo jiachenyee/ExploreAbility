b17e3849f74196d781cc3247528cc63c2283f6f2
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-05-15 07:37:32 +0700

    add statistics on locations groups are at

--- ExploreAbility Console/View/Private Window/PrivateControllerView.swift
+++ ExploreAbility Console/View/Private Window/PrivateControllerView.swift
@@ -40,9 +40,87 @@ struct PrivateControllerView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             
             if let roomCaptureData = viewModel.roomCaptureData {
-                SetNextChallengeView(roomCaptureData: roomCaptureData, beaconPositions: viewModel.beaconPositions)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                ScrollView {
+                    VStack(spacing: 0) {
+                        SetNextChallengeView(roomCaptureData: roomCaptureData, beaconPositions: viewModel.beaconPositions)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .aspectRatio(1, contentMode: .fit)
+                        Divider()
+                        Text("Beacons")
+                            .padding()
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        ForEach(0..<7) { beaconIndex in
+                            VStack(alignment: .leading) {
+                                
+                                Text("Beacon \(beaconIndex)")
+                                    .font(.title2)
+                                    .fontWeight(.medium)
+                                
+                                let (arrivals, at, departures) = getGroups(at: beaconIndex)
+                                
+                                HStack(alignment: .top) {
+                                    VStack {
+                                        Label("Arrivals", systemImage: "figure.walk.arrival")
+                                            .fontWeight(.bold)
+                                        ForEach(arrivals, id: \.self) {
+                                            Text($0)
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    
+                                    Divider()
+                                    VStack {
+                                        Label("At", systemImage: "figure.stand")
+                                            .fontWeight(.bold)
+                                        ForEach(at, id: \.self) {
+                                            Text($0)
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    Divider()
+                                    VStack {
+                                        Label("Departures", systemImage: "figure.walk.departure")
+                                            .fontWeight(.bold)
+                                        ForEach(departures, id: \.self) {
+                                            Text($0)
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                Divider()
+                            }
+                            .padding(.horizontal)
+                            .padding(.bottom)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                }
+                .frame(minWidth: 200)
             }
         }
+    }
+    
+    func getGroups(at beaconIndex: Int) -> ([String], [String], [String]) {
+        var arrivals: [String] = []
+        var at: [String] = []
+        var departures: [String] = []
+        
+        for group in viewModel.groups {
+            if group.currentBeacon == beaconIndex + 1 && (group.progress ?? 0.5) < 0.3 {
+                if group.currentState == .exploring {
+                    arrivals.append(group.name)
+                } else {
+                    arrivals.append(group.name)
+                }
+            } else if group.nextChallengeBeacon == beaconIndex + 1 {
+                departures.append(group.name)
+            }
+        }
+        
+        return (arrivals, at, departures)
     }
 }
